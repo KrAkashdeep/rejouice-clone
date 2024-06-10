c3d2961b51eed460ba4ee1fda2c8e832b8c56357
KrAkashdeep <akashdeep93140@gmail.com>
2024-06-11 00:37:47 +0530

    add initial file

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <video autoPlay loop muted src="src\assets\video.mp4" />
+        <div className="content">
+          <span>R</span>
+          <span>E</span>
+          <span>J</span>
+          <span>O</span>
+          <span>U</span>
+          <span>I</span>
+          <span>C</span>
+          <span>E</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
